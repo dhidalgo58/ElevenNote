ff90254293ac7bbaed33c9cec3e454b6d15a2cf5
dhidalgo58 <dhidalgo58@outlook.com>
2023-09-06 18:09:53 -0700

    migration and database setup

--- ElevenNote.WebAPI/Program.cs
+++ ElevenNote.WebAPI/Program.cs
@@ -2,6 +2,7 @@ using ElevenNote.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
